ea6febbdaaeaceb03cfbcd31cd6e421fc58ca2fc
Nikita <nvoitov@gmail.com>
2024-06-20 11:15:49 +0300

    Create replaceLinks.js

---
+++ replaceLinks.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.CenterEllipsisstyled__CenterEllipsis-rum-sc-1jyrjb6-0').forEach(function(element) {
+  if (element.textContent.includes('splunko11y.com/cart/checkout')) {
+    element.textContent = element.textContent.replace('splunko11y.com/cart/checkout', 'eclaire.com/revenue/reports');
+  }
+});
